5887523a0e66b8f37f6118fc7130a658423b3c87
cdoma <mcdoma.ojt@ayannah.com>
2016-08-31 16:46:27 +0800

    Update slack.js

--- slack.js
+++ slack.js
@@ -2,5 +2,5 @@ const	webhook = 	process.env.hook,
 	requestify =	require('requestify');
 
 exports.sendMessage = function(message){
-	requestify.post(webhook, message);
+	requestify.post('https://hooks.slack.com/services/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', lemme see);
 };
